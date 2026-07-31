a1ddbb26526165802ccf99e37c481146f2771c24
dafafauzan01 <dafafauzan792@gmail.com>
2026-07-31 13:43:42 +0700

    fix: memperbaiki tampilan foto produk pada halaman utama index blade

--- resources/views/produk/_form.blade.php
+++ resources/views/produk/_form.blade.php
@@ -1,98 +1,88 @@
-@csrf
+@csrf 
 
-@if (!empty($produk->foto))
-    <div class="mb-2">
-        <label>Foto Saat Ini</label><br>
-        <img src="{{ asset('storage/' . $produk->foto) }}" 
-             width="150"
-             class="img-thumbnail">
-    </div>
-@endif
+@if (!empty($produk->foto)) 
+<div class="mb-2"> 
+    <label>Foto Saat Ini</label><br> 
+    <img src="{{ asset('storage/' . $produk->foto) }}" width="150" class="img-thumbnail"> 
+</div> 
+@endif 
 
-<div class="row">
-    <div class="col">
-        <div>
-            <label>Gambar</label>
-            <input type="file"
-                   name="foto"
-                   onchange="previewImage(this)"
-                   class="form-control @error('foto') is-invalid @enderror"> 
-            @error('foto')
-                <div class="class="invalid-feedback d-block">
-                    {{ $message }}
-                </div>
+<div class="row"> 
+    <div class="col"> 
+        <div> 
+            <label>Gambar</label> 
+            <!-- name tetap 'foto' sesuai Controller -->
+            <input type="file" name="foto" onchange="previewImage(this)" class="form-control @error('foto') is-invalid @enderror"> 
+            @error('foto') 
+            <!-- Perbaikan: Menghapus tulisan double class=" -->
+            <div class="invalid-feedback d-block"> 
+                {{ $message }} 
+            </div> 
             @enderror 
-        </div>
-    </div>
-    <div class="col">
-        <div class="mb-2">
-            <label>Preview Foto</label><br>
-            <img id="preview" class="img-thumbnail mt-2" style="display:none" width="150">
-        </div>
-    </div>
-</div>
+        </div> 
+    </div> 
+    <div class="col"> 
+        <div class="mb-2"> 
+            <label>Preview Foto</label><br> 
+            <img id="preview" class="img-thumbnail mt-2" style="display:none" width="150"> 
+        </div> 
+    </div> 
+</div> 
 
-<div>
-    <label>Nama Produk</label><br>
-    <input type="text" name="name"
-           class="form-control @error('name') is-invalid @enderror"
-           value="{{ old('name', $produk->nama ?? '') }}">
-    @error('name')
-        <div class="invalid-feedback">
-            {{ $message }}
-        </div>
-    @enderror
-</div>
+<div> 
+    <label>Nama Produk</label><br> 
+    <!-- name dikembalikan ke 'name' sesuai isi Controller -->
+    <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name', $produk->nama ?? '') }}"> 
+    @error('name') 
+    <div class="invalid-feedback"> 
+        {{ $message }} 
+    </div> 
+    @enderror 
+</div> 
 
-<div>
-    <label>Harga Beli</label><br>
-    <input type="number" name="purchase_price"
-           class="form-control @error('purchase_price') is-invalid @enderror"
-           value="{{ old('purchase_price', $produk->harga_beli ?? '') }}">
-    @error('purchase_price')
-        <div class="invalid-feedback">
-            {{ $message }}
-        </div>
-    @enderror
-</div>
+<div> 
+    <label>Harga Beli</label><br> 
+    <!-- name dikembalikan ke 'purchase_price' sesuai isi Controller -->
+    <input type="number" name="purchase_price" class="form-control @error('purchase_price') is-invalid @enderror" value="{{ old('purchase_price', $produk->harga_beli ?? '') }}"> 
+    @error('purchase_price') 
+    <div class="invalid-feedback"> 
+        {{ $message }} 
+    </div> 
+    @enderror 
+</div> 
 
-<div>
-    <label>Harga Jual</label><br>
-    <input type="number" name="selling_price"
-           class="form-control @error('selling_price') is-invalid @enderror"
-           value="{{ old('selling_price', $produk->harga_jual ?? '') }}">
-    @error('selling_price')
-        <div class="invalid-feedback">
-            {{ $message }}
-        </div>
-    @enderror
-</div>
+<div> 
+    <label>Harga Jual</label><br> 
+    <!-- name dikembalikan ke 'selling_price' sesuai isi Controller -->
+    <input type="number" name="selling_price" class="form-control @error('selling_price') is-invalid @enderror" value="{{ old('selling_price', $produk->harga_jual ?? '') }}"> 
+    @error('selling_price') 
+    <div class="invalid-feedback"> 
+        {{ $message }} 
+    </div> 
+    @enderror 
+</div> 
 
-<div>
-    <label>Stok</label><br>
-    <input type="number" name="stock"
-           class="form-control @error('stock') is-invalid @enderror"
-           value="{{ old('stock', $produk->stok ?? '') }}">
-    @error('stock')
-        <div class="invalid-feedback">
-            {{ $message }}
-        </div>
-    @enderror
-</div>
+<div> 
+    <label>Stok</label><br> 
+    <!-- name dikembalikan ke 'stock' sesuai isi Controller -->
+    <input type="number" name="stock" class="form-control @error('stock') is-invalid @enderror" value="{{ old('stock', $produk->stok ?? '') }}"> 
+    @error('stock') 
+    <div class="invalid-feedback"> 
+        {{ $message }} 
+    </div> 
+    @enderror 
+</div> 
 
-<button class="btn btn-success mt-3" type="submit">Simpan</button>
-<a href="{{ route('produk.index') }}" class="btn btn-secondary mt-3">Kembali</a>
+<button class="btn btn-success mt-3" type="submit">Simpan</button> 
+<a href="{{ route('produk.index') }}" class="btn btn-secondary mt-3">Kembali</a> 
 
-<script>
-function previewImage(input) {
-    const preview = document.getElementById('preview');
-    const file = input.files[0];
-
-    if (file) {
-        preview.src = URL.createObjectURL(file);
-        preview.style.display = 'block';
-    }
-}
+<script> 
+function previewImage(input) { 
+    const preview = document.getElementById('preview'); 
+    if (input.files && input.files[0]) { 
+        const file = input.files[0];
+        preview.src = URL.createObjectURL(file); 
+        preview.style.display = 'block'; 
+    } 
+} 
 </script>
-
-
